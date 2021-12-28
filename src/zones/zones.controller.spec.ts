bc09be8a5b4ae226181ba020a1cb11b31dcd7506
vorrapong <vorrapong@sirisoft.co.th>
2021-12-28 15:06:47 +0700

    add: test ZonesController

--- src/zones/zones.controller.spec.ts
+++ src/zones/zones.controller.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ZonesController } from './zones.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ZonesController } from "./zones.controller";
+import { Zone } from "./zones.entity";
+import { ZonesService } from "./zones.service";
 
-describe('ZonesController', () => {
+describe("ZonesController", () => {
   let controller: ZonesController;
+  let fakeZonesService: Partial<ZonesService>;
 
   beforeEach(async () => {
+    const zones: Zone[] = [];
+    fakeZonesService = {
+      getAllZones: () => {
+        return Promise.resolve([
+          { name: "nabu", detail: "star war planet" } as Zone,
+        ]);
+      },
+      create: (name: string, detail: string) => {
+        return Promise.resolve({ id: 1, name, detail } as Zone);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ZonesController],
+      providers: [
+        {
+          provide: ZonesService,
+          useValue: fakeZonesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ZonesController>(ZonesController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("zones should be defined when get zones", async () => {
+    const zones = await controller.getZones();
+    expect(zones).toBeDefined();
+  });
+
+  it("return zone when create zone", async () => {
+    const zone = await controller.createZone({
+      id: 1,
+      name: "nabu",
+      detail: "star watr planet",
+    } as Zone);
+    expect(zone.name).toEqual("nabu");
+    expect(zone.detail).toEqual("star watr planet");
   });
 });
--- src/zones/zones.service.spec.ts
+++ src/zones/zones.service.spec.ts
@@ -1,18 +1,59 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ZonesService } from './zones.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { User } from "src/users/users.entity";
+import { UsersService } from "src/users/users.service";
+import { ZonesService } from "./zones.service";
 
-describe('ZonesService', () => {
+describe("ZonesService", () => {
   let service: ZonesService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      findById: (id: number) => {
+        const filteredUsers = users.filter((user) => user.id === id);
+        return Promise.resolve(filteredUsers[0]);
+      },
+      createUser: (
+        email: string,
+        password: string,
+        firstName: string,
+        lastName: string
+      ) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+          firstName,
+          lastName,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ZonesService],
+      providers: [
+        ZonesService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<ZonesService>(ZonesService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
+
+  // it("return zone instance when create zone", async () => {
+  //   const zone = await service.create("nabu", "star war planet");
+  //   expect(zone).toBeDefined();
+  // });
 });
