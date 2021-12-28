d28ff98e944a990e109567c37e9069a43a243e09
vorrapong <vorrapong@sirisoft.co.th>
2021-12-28 11:26:10 +0700

    add: more authService

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { User } from "../users.entity";
 import { UsersService } from "../users.service";
-
 describe("AuthService", () => {
   let service: AuthService;
   let fakeUsersService: Partial<UsersService>;
@@ -45,6 +44,10 @@ describe("AuthService", () => {
     service = module.get(AuthService);
   });
 
+  it("can create an instance of auth service", async () => {
+    expect(service).toBeDefined();
+  });
+
   it("creates a new user with a salted and hashed password", async () => {
     const user = await service.register("10@email.com", "12345", "1", "1");
 
@@ -54,16 +57,36 @@ describe("AuthService", () => {
     expect(hash).toBeDefined();
   });
 
-  it("can create an instance of auth service", async () => {
-    expect(service).toBeDefined();
+  it("throws an error if user register with email that is in use", async () => {
+    await service.register("1@email.com", "12345", "1", "1");
+    try {
+      await service.register("1@email.com", "12345", "1", "1");
+    } catch (error) {
+      expect(error.message).toEqual("email in use");
+    }
   });
 
-  // it("throws an error if user signs up with email that is in use", async (done) => {
-  //   await service.register("1@email.com", "12345", "1", "1");
-  //   try {
-  //     await service.register("1@email.com", "12345", "1", "1");
-  //   } catch (err) {
-  //     done();
-  //   }
-  // });
+  it("throw error message userEmail: ${email} not found if not found email", async () => {
+    const email = "1@email.com";
+    try {
+      await service.logIn(email, "12345");
+    } catch (error) {
+      expect(error.message).toEqual(`userEmail: ${email} not found`);
+    }
+  });
+
+  it("return user if login success", async () => {
+    await service.register("1@email.com", "12345", "1", "1");
+    const user = await service.logIn("1@email.com", "12345");
+    expect(user).toBeDefined();
+  });
+
+  it("throw error message if wrong password", async () => {
+    await service.register("1@email.com", "12345", "1", "1");
+    try {
+      await service.logIn("1@email.com", "12345");
+    } catch (error) {
+      expect(error.message).toEqual(`wrong password`);
+    }
+  });
 });
