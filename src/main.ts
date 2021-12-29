5fc190d5843b4705f81646ae26c930c9ef2b5bed
vorrapong <vorrapong@sirisoft.co.th>
2021-12-29 13:59:50 +0700

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
     res.removeHeader("date");
     next();
   });
+
+  const config = new DocumentBuilder()
+    .setTitle("Event example")
+    .setDescription("The events API description")
+    .setVersion("1.0")
+    .addTag("events")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
